c5f37870c9248ea582291f603e7806d83e9c2f6c
Saaba <nathan.albino01@gmail.com>
2025-01-21 09:20:48 -0500

    add tree generator
    using perlin noise, need to fix sorting layer of generated trees

--- Assets/Map_Generation/MapGenerator.cs
+++ Assets/Map_Generation/MapGenerator.cs
@@ -9,9 +9,14 @@ namespace Prototype_S
         [SerializeField] private int mapWidth;
         [SerializeField] private int mapHeight;
         [SerializeField] private float scale;
+        [SerializeField] private float treeScale;
+        [SerializeField] private float treeDensity = 0.25f;
 
+        [SerializeField] private Tilemap foliageMap;
         [SerializeField] private Tilemap terrainMap; // Reference to the Tilemap
         [SerializeField] private Tilemap waterMap; // Reference to the Tilemap
+
+        [SerializeField] private TileBase treeTile;
         [SerializeField] private TileBase waterTile; // Tile for water
         [SerializeField] private TileBase grassTile; // Tile for grass
         [SerializeField] private TileBase sandTile; // Tile for mountains
@@ -22,8 +27,10 @@ namespace Prototype_S
             terrainMap.ClearAllTiles();
             waterMap.ClearAllTiles();
             
+            //generate terrain noise map
             float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale);
 
+            //fill terrain tiles based on noise map
             for (int y = 0; y < mapHeight; y++)
             {
                 for (int x = 0; x < mapWidth; x++)
@@ -53,5 +60,36 @@ namespace Prototype_S
             
         }
 
+        [ContextMenu("Generate Trees")]
+        private void TreeGenerator()
+        {
+            
+            foliageMap.ClearAllTiles();
+            float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, treeScale);
+            
+            //generate trees based on noise map
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+
+                    float density = Random.Range(0.05f, treeDensity);
+                    if (!isWater(x,y) && !isSand(x,y) && noiseMap[x,y] < density)
+                    {
+                        foliageMap.SetTile(new Vector3Int(x, y, 0), treeTile);
+                    }
+                }
+            }
+        }
+
+        private bool isWater(int x, int y)
+        {
+            return (waterMap.GetTile(new Vector3Int(x, y, 0)) == waterTile);
+        }
+
+        private bool isSand(int x, int y)
+        {
+            return (terrainMap.GetTile(new Vector3Int(x, y, 0)) == sandTile);
+        }
     }
 }
